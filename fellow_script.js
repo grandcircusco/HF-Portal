9556761289994a25d9024ec9e70ec10a0d59e534
Cam <camherringshaw@gmail.com>
2015-07-23 11:19:10 -0400

    Drawing card fields from controller
    Starting to fetch data for the cards from the controller rather than
    having it hard-coded. Hopefully, that will make it easier to fetch data
    from the database when we get to that point.

--- fellow_script.js
+++ fellow_script.js
@@ -1,12 +1,21 @@
 var app = angular.module('fellow_app', []);
 
 app.controller('fellow_controller', function($scope) {
-	$scope.fellows = ['James York', 'Alex Suriano', 'Cam Herringshaw', 'Major Sapp'];
+	$scope.fellows = [{
+		name: 'James York'
+	}, {
+		name: 'Alex Suriano'
+	}, {
+		name: 'Cam Herringshaw'
+	}, {
+		name: 'Major Sapp'
+	}];
 });
 
 app.directive('fellowCard', function() {
 	return {
 		replace: true,
+		scope: true,
 		templateUrl: '/src/views/partials/fellow_card.html'
 	};
 });
