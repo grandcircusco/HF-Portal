2e401b4d4d90e776097089eb8605dbca969b9ee7
Cam <camherringshaw@gmail.com>
2015-07-23 10:59:43 -0400

    Fellow card directive is working
    Using ng-repeat and directive fellowCard. About to try using a list of
    objects instead of a list of names for fellows.

--- fellow_script.js
+++ fellow_script.js
@@ -3,3 +3,10 @@ var app = angular.module('fellow_app', []);
 app.controller('fellow_controller', function($scope) {
 	$scope.fellows = ['James York', 'Alex Suriano', 'Cam Herringshaw', 'Major Sapp'];
 });
+
+app.directive('fellowCard', function() {
+	return {
+		replace: true,
+		templateUrl: '/src/views/partials/fellow_card.html'
+	};
+});
