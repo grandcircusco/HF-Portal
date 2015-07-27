6890f5314cada6eba2225c4473a12ba77ef780ba
Ken Dunlap <kdunlap4918@gmail.com>
2015-07-27 14:37:20 -0400

    Changed routes in app.js

--- app.js
+++ app.js
@@ -4,54 +4,44 @@
  * @desc    contains the routes for the app
  */
 
-(function () {
-  'use strict';
+var app = angular.module('app',['ngRoute']);
 
-  angular
-    .module('app',
-      'app.fellows',
-      'app.companies',
-      ['ngRoute']);
-    .config(config);
+/**
+ *   * @name config
+ *     * @desc Define valid application routes
+ *       */
+app.config(function($routeProvider){
+    $routeProvider
+        .when('/', {
+            templateUrl : 'source/app/home/home.html',
+            controller  : 'routingController'
+        })
+        .when('/fellows', {
+            controller: 'routingController',
+            templateUrl: 'source/app/fellows/fellows.html'
+        })
+        .when('/companies', {
+            controller: 'routingController',
+            templateUrl: 'source/app/companies/company_view.html'
+        })
+        .when('/register', {
+            controller: 'routingController',
+            templateUrl: 'register.html'
+        })
+        .when('/fellow/:id', {
+            controller: 'routingController',
+            templateUrl: 'source/app/fellows/fellow_detail_view.html'
+        })
+        .when('/company/:id', {
+            controller: 'routingController',
+            templateUrl: 'source/app/companies/company_detail_view.html'
+        })
+        .otherwise({ redirectTo: '/' });
 
-  config.$inject = ['$routeProvider'];
+});
 
-  /**
-   *   * @name config
-   *     * @desc Define valid application routes
-   *       */
-  function config($routeProvider) {
-	$routeProvider
-     .when('/', {
-                templateUrl : 'source/app/home/home.html',
-                controller  : 'routingController'
-      })
-      .when('/fellows', {
-        controller: 'routingController',
-        templateUrl: 'source/app/fellows/fellows.html'
-      })
-      .when('/companies', {
-        controller: 'routingController',
-        templateUrl: 'source/app/companies/company_view.html'
-      })
-      .when('/register', {
-        controller: 'routingController',
-        templateUrl: 'register.html'
-      })
-      .when('/fellow/:id', {
-        controller: 'routingController',
-        templateUrl: 'source/app/fellows/fellow_detail_view.html'
-      })
-      .when('/company/:id', {
-        controller: 'routingController',
-        templateUrl: 'source/app/companies/company_detail_view.html'
-      })
-      .otherwise({ redirectTo: '/' });
-
-  }
-
-})();
 
 app.controller('routingController', function() {
 
 });
+
