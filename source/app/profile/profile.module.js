488198b300f1d071052635a61a248ce473121c00
Kyle Mills <kdmills@mtu.edu>
2015-07-30 10:15:02 -0400

    added the profile folder with fake files

---
+++ source/app/profile/profile.module.js
@@ -0,0 +1,22 @@
+/**
+ * home module
+ */
+
+(function () {
+  'use strict';
+
+  angular
+    .module('app.home', [
+        'app.home.controllers',
+        'app.home.services'
+        ]);
+
+  //declare the controllers module
+  angular
+    .module('app.home.controllers', []);
+
+  //declare the directives module
+  angular
+    .module('app.home.directives', []);
+
+})();
