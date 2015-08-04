3c1482dee1b548a29b11eb41d862b49b2c060a28
Cam H <camherringshaw@gmail.com>
2015-08-04 11:01:12 -0400

    Merged with master, modified database (no salt column necessary). Doesn't actually deny users access yet.

--- source/models/User.js
+++ source/models/User.js
@@ -6,8 +6,7 @@ module.exports = function(sequelize, DataTypes) {
 
         id:			{ type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
         email: 		{ type: DataTypes.STRING, allowNull: false },
-        password: 	{ type: DataTypes.STRING, allowNull: false },
-        salt:		{ type: DataTypes.STRING, /*allowNull: false*/ }
+        password: 	{ type: DataTypes.STRING, allowNull: false }
     });
 
     return User;
