1fa158a73e8eacf6f312e5c127bba8f76c82083d
Cam H <camherringshaw@gmail.com>
2015-08-06 13:09:28 -0400

    Directing gzippo to a different directory (hopefully the right one this time).

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
-app.use(gzippo.staticGzip(__dirname + "/public"));
+app.use(gzippo.staticGzip(__dirname));
 appluse(gzippo.compress());
 
 
