7d12d2250e42857adda600decf9dcea1fbf5d9f5
Ken Dunlap <kdunlap4918@gmail.com>
2016-02-11 15:38:33 -0500

    Attempting to fix heroku sequelize issues

--- source/models/index.js
+++ source/models/index.js
@@ -11,17 +11,20 @@
 
     var sequelize = null;
     if (process.env.DATABASE_URL) {
+
         // the application is executed on Heroku ... use the postgres database
         sequelize = new Sequelize(process.env.DATABASE_URL, {
             dialect:  'postgres',
             protocol: 'postgres',
-            port:     match[4],
-            host:     match[3],
+            //port:     match[4],
+            //host:     match[3],
             logging:  true //false
-        })
+        });
+
     } else {
+
         // the application is executed on the local machine ... use mysql
-        sequelize = new Sequelize( "postgres://localhost:5432/hfportal" )
+        sequelize = new Sequelize( "postgres://localhost:5432/hfportal" );
     }
 
     var db        = {};
